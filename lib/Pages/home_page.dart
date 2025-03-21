a3d1840ed7a864b9aaf9924228c0fab9ae55eaff
prospermbuma <prospermbuma@gmail.com>
2025-03-21 09:45:32 +0300

    More updates

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mental_health_app/util/exercise_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,109 +14,230 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
-        statusBarColor: Colors.blue[800], // Match status bar color with background
-        statusBarIconBrightness: Brightness.light, // Light icons for dark background
+        statusBarColor: Colors.blue[800],
+        // Match status bar color with background
+        statusBarIconBrightness:
+            Brightness.light, // Light icons for dark background
       ),
       child: Scaffold(
         backgroundColor: Colors.blue[800],
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30, color: Colors.blue[800]),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.window, size: 30, color: Colors.blue[800]),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.email, size: 30, color: Colors.blue[800]),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, size: 30, color: Colors.blue[800]),
+              label: '',
+            ),
+          ],
+        ),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              children: [
-                /* ====== Greetings row ====== */
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Column(
                   children: [
-                    /* ====== Hi Prosper! ====== */
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    /* ====== Greetings row ====== */
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          'Hi, Prosper!',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        /* ====== Hi Prosper! ====== */
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Hi, Prosper!',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              '20 Mar, 2025',
+                              style: TextStyle(color: Colors.blue[100]),
+                            ),
+                          ],
                         ),
-                        SizedBox(height: 8),
-                        Text(
-                          '20 Mar, 2025',
-                          style: TextStyle(color: Colors.blue[100]),
+
+                        /* ====== Notification ====== */
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue[600],
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(12),
+                            child: Icon(
+                              Icons.notifications,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ],
                     ),
 
-                    /* ====== Notification ====== */
+                    SizedBox(height: 25),
+
+                    /* ====== Search bar ====== */
                     Container(
                       decoration: BoxDecoration(
                         color: Colors.blue[600],
                         borderRadius: BorderRadius.circular(12),
                       ),
                       child: Padding(
-                        padding: EdgeInsets.all(12),
-                        child: Icon(Icons.notifications, color: Colors.white),
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.search, color: Colors.white),
+                            SizedBox(width: 5),
+                            Text(
+                              'Search',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ],
-                ),
 
-                SizedBox(height: 25),
+                    SizedBox(height: 25),
 
-                /* ====== Search bar ====== */
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue[600],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(
+                    /* ====== How do you feel ====== */
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(Icons.search, color: Colors.white),
-                        SizedBox(width: 5),
-                        Text('Search', style: TextStyle(color: Colors.white)),
+                        Text(
+                          'How do you feel?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "••",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
                       ],
                     ),
-                  ),
+
+                    SizedBox(height: 25),
+
+                    /* ====== How do you feel icons ====== */
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildEmotionTile("😫", "Badly"),
+                        _buildEmotionTile("😊", "Fine"),
+                        _buildEmotionTile("😁", "Well"),
+                        _buildEmotionTile("😀", "Excellent"),
+                      ],
+                    ),
+                  ],
                 ),
+              ),
 
-                SizedBox(height: 25),
+              SizedBox(height: 25),
 
-                /* ====== How do you feel ====== */
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'How do you feel?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25), // Left border radius
+                      topRight: Radius.circular(25), // Right border radius
+                    ),
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 25,
+                      ),
+                      child: Column(
+                        /* ==== Exercises heading ==== */
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Exercises',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "••",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.grey[900],
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          SizedBox(height: 20),
+
+                          /* ==== Listview of exercises ==== */
+                          Expanded(
+                            child: ListView(
+                              children: [
+                                ExerciseTile(
+                                  icon: Icon(
+                                    Icons.favorite,
+                                    color: Colors.white,
+                                  ),
+                                  iconBgColor: Colors.deepOrange,
+                                  exerciseName: 'Speaking Skills',
+                                  numberOfExercise: '16 Exercises',
+                                ),
+                                ExerciseTile(
+                                  icon: Icon(Icons.person, color: Colors.white),
+                                  iconBgColor: Colors.blue[800],
+                                  exerciseName: 'Reading Speed',
+                                  numberOfExercise: '8 Exercises',
+                                ),
+                                ExerciseTile(
+                                  icon: Icon(
+                                    Icons.book_outlined,
+                                    color: Colors.white,
+                                  ),
+                                  iconBgColor: Colors.pink,
+                                  exerciseName: 'Writing Speed',
+                                  numberOfExercise: '10 Exercises',
+                                ),
+                                ExerciseTile(
+                                  icon: Icon(
+                                    Icons.fitness_center,
+                                    color: Colors.white,
+                                  ),
+                                  iconBgColor: Colors.deepPurple,
+                                  exerciseName: 'Yoga Poses',
+                                  numberOfExercise: '10 Exercises',
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Text(
-                      "••",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  ],
+                  ),
                 ),
-
-                SizedBox(height: 25),
-
-                /* ====== How do you feel icons ====== */
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _buildEmotionTile("😫", "Badly"),
-                    _buildEmotionTile("😊", "Fine"),
-                    _buildEmotionTile("😁", "Well"),
-                    _buildEmotionTile("😀", "Excellent"),
-                  ],
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
---
+++ lib/util/exercise_tile.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTile extends StatelessWidget {
+  // Instance Variables
+  final Icon icon;
+  final Color? iconBgColor; // Allow nullable Color
+  final String exerciseName;
+  final String numberOfExercise;
+
+  // Constructor
+  const ExerciseTile({
+    super.key,
+    required this.icon,
+    required this.iconBgColor,
+    required this.exerciseName,
+    required this.numberOfExercise,
+  });
+
+  // Polymorphism in action
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: iconBgColor ?? Colors.blue, // Provide default color if null
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: icon, // Dynamically used icon here
+                ),
+                SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Title
+                    Text(
+                      exerciseName, // Use dynamic exerciseName
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    // Subtitle
+                    Text(
+                      numberOfExercise, // Use dynamic numberOfExercise
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 22),
+              child: Text(
+                "••",
+                style: TextStyle(fontSize: 20, color: Colors.grey[900]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
